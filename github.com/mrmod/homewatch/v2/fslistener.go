efb5fa1dca68fc52a198433660923a834021b264
Bruce McMoran <mcmoranbjr@gmail.com>
2024-02-13 18:45:06 -0800

    27 Watch child paths on startup
    When each path in `--v2-watch-paths` is walked the child paths which are directories are added to the FSNotify watcher.
    
    ultralist:27

--- github.com/mrmod/homewatch/v2/fslistener.go
+++ github.com/mrmod/homewatch/v2/fslistener.go
@@ -1,10 +1,38 @@
 package v2
 
 import (
+	"io/fs"
 	"log"
+	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
 )
+
+func tryAddPath(w *fsnotify.Watcher, root string) error {
+	if err := w.Add(root); err != nil {
+		log.Printf("WARN: Error watching '%s': %s", root, err)
+		return err
+	}
+	log.Printf("INFO: Added watch on '%s'", root)
+	return nil
+}
+func AddPaths(w *fsnotify.Watcher, root string) error {
+	errorPaths := []string{}
+	walkFun := func(path string, d fs.DirEntry, err error) error {
+		if !d.IsDir() {
+			log.Printf("DEBUG: Skipping file %s", path)
+			return nil
+		}
+		if err := tryAddPath(w, path); err != nil {
+			log.Printf("DEBUG: Error walking %s: %s", path, err)
+			errorPaths = append(errorPaths, path)
+			return err
+		}
+		log.Printf("DEBUG: Walking %s", path)
+		return nil
+	}
+	return filepath.WalkDir(root, walkFun)
+}
 
 func Listen(paths ...string) {
 	watcher, err := fsnotify.NewWatcher()
@@ -35,11 +63,14 @@ func Listen(paths ...string) {
 		}
 	}()
 	for _, path := range paths {
-		err = watcher.Add(path)
-		if err != nil {
-			panic(err)
+		// TODO: Track which paths are watched
+		// TODO: Should get list of child paths each iteration
+		// TODO: Should add child paths if they are not yet watched
+		// TODO: Should remove watched paths if they get no events for 24 hours
+
+		if err := AddPaths(watcher, path); err != nil {
+			log.Printf("WARN: Error watching %s: %s", path, err)
 		}
-		log.Printf("Watching: %s", path)
 	}
 	<-done
 }
